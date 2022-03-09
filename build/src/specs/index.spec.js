255c4450f8e3b741fe794438b991a364d135b87d
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-09 11:07:34 -0800

    added some more complex tests, needs recursion-to-reduce pr to go through first

--- build/src/specs/index.spec.js
+++ build/src/specs/index.spec.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const editDistance_1 = require("../editDistance");
 describe('edit_distance', () => {
     it('case: 1', () => {
-        const result = (0, editDistance_1.main)('justin', 'jusin');
+        const result = (0, editDistance_1.edit_distance)('justin', 'jusin');
         const expectedResult = [
             {
                 errorString: 't',
@@ -17,7 +17,7 @@ describe('edit_distance', () => {
         expect(result).toEqual(expectedResult);
     });
     it('case: 2', () => {
-        const result = (0, editDistance_1.main)('justpfin', 'jusin');
+        const result = (0, editDistance_1.edit_distance)('justpfin', 'jusin');
         const expectedResult = [
             {
                 errorString: 'tpf',
@@ -31,7 +31,7 @@ describe('edit_distance', () => {
         expect(result).toEqual(expectedResult);
     });
     it('case: 3', () => {
-        const result = (0, editDistance_1.main)('jusin', 'justin');
+        const result = (0, editDistance_1.edit_distance)('jusin', 'justin');
         const expectedResult = [
             {
                 errorString: 't',
@@ -45,7 +45,7 @@ describe('edit_distance', () => {
         expect(result).toEqual(expectedResult);
     });
     it('case: 4', () => {
-        const result = (0, editDistance_1.main)('jusin', 'justpfin');
+        const result = (0, editDistance_1.edit_distance)('jusin', 'justpfin');
         const expectedResult = [
             {
                 errorString: 'tpf',
@@ -58,16 +58,83 @@ describe('edit_distance', () => {
         ];
         expect(result).toEqual(expectedResult);
     });
-    it('case: 5', () => {
-        const result = (0, editDistance_1.main)('ab', 'ac');
+    // it('case: 5', () => {
+    //   const result = edit_distance('ab', 'ac');
+    //   const expectedResult = [
+    //     {
+    //       errorString: 'b',
+    //       startIndex: 1,
+    //       endIndex: 2,
+    //       genIndices: [1],
+    //       expIndices: [1],
+    //       operation: 'replace',
+    //     },
+    //   ];
+    //   expect(result).toEqual(expectedResult);
+    // });
+    // it('case: 6', () => {
+    //   const result = edit_distance('abf', 'acq');
+    //   const expectedResult = [
+    //     {
+    //       errorString: 'bf',
+    //       startIndex: 1,
+    //       endIndex: 3,
+    //       genIndices: [1, 2],
+    //       expIndices: [1, 2],
+    //       operation: 'replace',
+    //     },
+    //   ];
+    //   expect(result).toEqual(expectedResult);
+    // });
+    // it('case: 7', () => {
+    //   const result = edit_distance(
+    //     'the dog and the bird',
+    //     'the cat and the bird'
+    //   );
+    //   const expectedResult = [
+    //     {
+    //       errorString: 'dog',
+    //       startIndex: 4,
+    //       endIndex: 7,
+    //       genIndices: [4, 5, 6],
+    //       expIndices: [4, 5, 6],
+    //       operation: 'replace',
+    //     },
+    //   ];
+    //   expect(result).toEqual(expectedResult);
+    // });
+    it('case: 7', () => {
+        const result = (0, editDistance_1.edit_distance)('the dog and the bird', 'the and the bird');
         const expectedResult = [
             {
-                errorString: 'b',
-                startIndex: 1,
-                endIndex: 2,
-                genIndices: [1],
-                expIndices: [1],
-                operation: 'replace',
+                errorString: ' dog',
+                startIndex: 3,
+                endIndex: 7,
+                genIndices: [3, 4, 5, 6],
+                expIndices: [2, 2, 2, 2],
+                operation: 'delete',
+            },
+        ];
+        expect(result).toEqual(expectedResult);
+    });
+    it('case: 8', () => {
+        const result = (0, editDistance_1.edit_distance)('the dog and the bird', 'the and the big bird');
+        const expectedResult = [
+            {
+                errorString: ' dog',
+                startIndex: 3,
+                endIndex: 7,
+                genIndices: [3, 4, 5, 6],
+                expIndices: [2, 2, 2, 2],
+                operation: 'delete',
+            },
+            {
+                errorString: ' big',
+                startIndex: 11,
+                endIndex: 15,
+                genIndices: [14, 14, 14, 14],
+                expIndices: [11, 12, 13, 14],
+                operation: 'insert',
             },
         ];
         expect(result).toEqual(expectedResult);
