78a1ded8fc295685cf209284397809cdb176e126
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-09 11:15:48 -0800

    adding more complex tests after rebasing

--- build/src/specs/index.spec.js
+++ build/src/specs/index.spec.js
@@ -72,5 +72,69 @@ describe('edit_distance', () => {
         ];
         expect(result).toEqual(expectedResult);
     });
+    it('case: 6', () => {
+        const result = (0, editDistance_1.edit_distance)('abf', 'acq');
+        const expectedResult = [
+            {
+                errorString: 'bf',
+                startIndex: 1,
+                endIndex: 3,
+                genIndices: [1, 2],
+                expIndices: [1, 2],
+                operation: 'replace',
+            },
+        ];
+        expect(result).toEqual(expectedResult);
+    });
+    it('case: 7', () => {
+        const result = (0, editDistance_1.edit_distance)('the dog and the bird', 'the cat and the bird');
+        const expectedResult = [
+            {
+                errorString: 'dog',
+                startIndex: 4,
+                endIndex: 7,
+                genIndices: [4, 5, 6],
+                expIndices: [4, 5, 6],
+                operation: 'replace',
+            },
+        ];
+        expect(result).toEqual(expectedResult);
+    });
+    it('case: 8', () => {
+        const result = (0, editDistance_1.edit_distance)('the dog and the bird', 'the and the bird');
+        const expectedResult = [
+            {
+                errorString: ' dog',
+                startIndex: 3,
+                endIndex: 7,
+                genIndices: [3, 4, 5, 6],
+                expIndices: [2, 2, 2, 2],
+                operation: 'delete',
+            },
+        ];
+        expect(result).toEqual(expectedResult);
+    });
+    it('case: 9', () => {
+        const result = (0, editDistance_1.edit_distance)('the dog and the bird', 'the and the big bird');
+        const expectedResult = [
+            {
+                errorString: ' dog',
+                startIndex: 3,
+                endIndex: 7,
+                genIndices: [3, 4, 5, 6],
+                expIndices: [2, 2, 2, 2],
+                operation: 'delete',
+            },
+            {
+                errorString: ' big',
+                startIndex: 11,
+                endIndex: 15,
+                genIndices: [14, 14, 14, 14],
+                expIndices: [11, 12, 13, 14],
+                operation: 'insert',
+            },
+        ];
+        expect(result).toEqual(expectedResult);
+    });
 });
 //# sourceMappingURL=index.spec.js.map
